f48380c5f85678fa2f28055259258f5731fe2a3e
barianbarbar <barianbarbar99@gmail.com>
2022-07-20 08:17:00 +0700

    Testing Jalan Workflow

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World Tes</h1>");
+    res.send("<h1>Hello World 1</h1>");
 });
 
 app.listen(3000, () => {
