0b14eebd6fa0b9522c450fa3f113eecebc84a170
barianbarbar <barianbarbar99@gmail.com>
2022-07-20 08:27:00 +0700

    Update Content to center

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World 1</h1>");
+    res.send("<center><h1>Hello World 1</h1></center>");
 });
 
 app.listen(3000, () => {
