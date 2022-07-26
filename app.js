595752096ff919ccd51b935bb713e8aae70b27b4
barianbarbar <barianbarbar99@gmail.com>
2022-07-26 14:59:46 +0700

    git tes push

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 // const mongoose = require("mongoose");
 // const bodyParser = require("body-parser");
 const app = express();
-const taskRouter = require("./routes/task");
+// const taskRouter = require("./routes/task");
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
-app.use("/", taskRouter);
+// app.use("/", taskRouter);
 
 // const db_atlas = "mongodb+srv://user-radea:radeakui12@cluster0.hri4h.mongodb.net/taskList";
 // const db_local = "mongodb://mongo:27017/taskList";
@@ -17,6 +17,9 @@ app.use("/", taskRouter);
 // .then(() => {
 //     console.log("Status Connected...")
 // });
+app.get("/", (req, res) => {
+    res.send("<h1>Hello</h1>");
+})
 
 app.listen(4000, () => {
     console.log("Server running with port 3000");
