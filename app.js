d0e2b4bc2998e18b83ba50f4928f529089f9c4fc
barianbarbar <barianbarbar99@gmail.com>
2022-07-26 17:47:28 +0700

    update routing

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 // const mongoose = require("mongoose");
 // const bodyParser = require("body-parser");
 const app = express();
-// const taskRouter = require("./routes/task");
+const taskRouter = require("./routes/task");
 
-// app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
-// app.use("/", taskRouter);
+app.use("/", taskRouter);
 
 // const db_atlas = "mongodb+srv://user-radea:radeakui12@cluster0.hri4h.mongodb.net/taskList";
 // const db_local = "mongodb://mongo:27017/taskList";
