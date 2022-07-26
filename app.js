9d88ca3441c0600206f56942dec53b2dce068a23
barianbarbar <barianbarbar99@gmail.com>
2022-07-26 20:53:12 +0700

    git return to default script

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
-// const taskRouter = require("./routes/task");
-const newRoute = require("./routesNew/routen");
+const taskRouter = require("./routes/task");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
-app.use("/", newRoute);
+app.use("/", taskRouter);
 
 const db_atlas = "mongodb+srv://user-radea:radeakui12@cluster0.hri4h.mongodb.net/taskList";
 const db_local = "mongodb://mongo:27017/taskList";
 
-mongoose.connect(db_atlas)
+mongoose.connect(db_local)
 .then(() => {
-    console.log("Status Connected...")
+    console.log("Status Connected...");
 });
 
 app.listen(4000, () => {
