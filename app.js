cd21d2326f8289a54a0a298b19a16ba15e5b329c
barianbarbar <barianbarbar99@gmail.com>
2022-07-26 14:56:00 +0700

    test

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 // const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const app = express();
 const taskRouter = require("./routes/task");
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
 app.use("/", taskRouter);
 
-const db_atlas = "mongodb+srv://user-radea:radeakui12@cluster0.hri4h.mongodb.net/taskList";
-const db_local = "mongodb://mongo:27017/taskList";
+// const db_atlas = "mongodb+srv://user-radea:radeakui12@cluster0.hri4h.mongodb.net/taskList";
+// const db_local = "mongodb://mongo:27017/taskList";
 
-mongoose.connect(db_local)
-.then(() => {
-    console.log("Status Connected...")
-});
+// mongoose.connect(db_local)
+// .then(() => {
+//     console.log("Status Connected...")
+// });
 
 app.listen(4000, () => {
     console.log("Server running with port 3000");
