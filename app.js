5efc813dc1ed2892652eae77621ce0d5112a8e68
barianbarbar <barianbarbar99@gmail.com>
2022-07-26 18:45:20 +0700

    ganti routing

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require("express");
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
-const taskRouter = require("./routes/task");
+// const taskRouter = require("./routes/task");
+const newRoute = require("./routesNew/routen");
 
 app.set("view engine", "ejs");
-// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
-// app.use("/", taskRouter);
+app.use("/", newRoute);
 
-// const db_atlas = "mongodb+srv://user-radea:radeakui12@cluster0.hri4h.mongodb.net/taskList";
-// const db_local = "mongodb://mongo:27017/taskList";
+const db_atlas = "mongodb+srv://user-radea:radeakui12@cluster0.hri4h.mongodb.net/taskList";
+const db_local = "mongodb://mongo:27017/taskList";
 
-// mongoose.connect(db_local)
-// .then(() => {
-//     console.log("Status Connected...")
-// });
+mongoose.connect(db_atlas)
+.then(() => {
+    console.log("Status Connected...")
+});
 
 app.listen(4000, () => {
     console.log("Server running with port 3000");
